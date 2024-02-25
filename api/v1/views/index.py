131cfe80115e4b4343be57e4f264d2ba381cb115
cwaita <cwaitanobongoza@gmail.com>
2024-02-25 13:44:30 -0800

    updated

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,8 +2,17 @@
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
-@app_views.route('/status', methods=['GET'])
-def status():
-    """Route to check API status"""
-    return jsonify({"status": "OK"})
+@app_views.route('/api/v1/stats', methods=['GET'])
+def get_stats():
+    """Route to retrieve the number of each object type"""
+    counts = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(counts)
