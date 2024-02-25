fc31398de5872d344f977242c3364136ed956ff4
cwaita <cwaitanobongoza@gmail.com>
2024-02-24 23:57:39 -0800

    place.py

--- models/place.py
+++ models/place.py
@@ -1,7 +1,20 @@
 #!/usr/bin/python3
-"""Defines the Place class."""
-from models.base_model import BaseModel
 
+from models.base_model import BaseModel
+from sqlalchemy import Table, Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+
+# Define the association table for place-amenity relationship
+place_amenity = Table('place_amenity', BaseModel.metadata,
+                      Column('place_id', String(60),
+                             ForeignKey('places.id'),
+                             primary_key=True,
+                             nullable=False),
+                      Column('amenity_id', String(60),
+                             ForeignKey('amenities.id'),
+                             primary_key=True,
+                             nullable=False)
+                      )
 
 class Place(BaseModel):
     """Represent a place.
