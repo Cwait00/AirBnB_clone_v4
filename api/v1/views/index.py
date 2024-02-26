f118936e8e3b0b54406c1cd96984258a6df1a70f
cwaita <cwaitanobongoza@gmail.com>
2024-02-26 03:56:53 -0800

    Add endpoint to retrieve object counts

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,11 +1,20 @@
-# api/v1/views/index.py
-
 #!/usr/bin/python3
-
-from flask import jsonify
+"""
+Index view
+"""
 from api.v1.views import app_views
+from flask import jsonify
+from models import storage
 
-@app_views.route('/status', methods=['GET'])
-def status():
-    """Return status OK"""
-    return jsonify({"status": "OK"})
+@app_views.route('/api/v1/stats', methods=['GET'])
+def get_stats():
+    """Retrieves the number of each object by type"""
+    stats = {
+        'amenities': storage.count('Amenity'),
+        'cities': storage.count('City'),
+        'places': storage.count('Place'),
+        'reviews': storage.count('Review'),
+        'states': storage.count('State'),
+        'users': storage.count('User')
+    }
+    return jsonify(stats)
