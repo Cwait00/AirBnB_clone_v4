ef0f9f40107b5c0ad1ceb5bf427b3291293e1aa2
cwaita <cwaitanobongoza@gmail.com>
2024-02-26 01:04:32 -0800

    Committing changes before switching branches

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-import os  # Import the os module
-from flask import Flask
+import os
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 
@@ -15,6 +15,11 @@ app.register_blueprint(app_views, url_prefix="/api/v1")
 @app.teardown_appcontext
 def teardown_appcontext(exception):
     storage.close()
+
+# Handler for 404 errors
+@app.errorhandler(404)
+def not_found_error(error):
+    return jsonify({"error": "Not found"}), 404
 
 if __name__ == "__main__":
     # Get host and port from environment variables, default to 0.0.0.0:5000
